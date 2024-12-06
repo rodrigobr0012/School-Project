711a70d1531d6f41d9e68045b0d5f1cf53552043
Rodrigo Macedo <workrodmac@gmail.com>
2024-12-06 04:52:21 -0300

    new commit

---
+++ Challenge_frontend/toodolist/app/components/AddTask.tsx
@@ -0,0 +1,12 @@
+import { FaPlus } from "react-icons/fa";
+
+const AddTask = () => {
+  return (
+    <div>
+      <button className="btn btn-prisma w-full">
+        Add new Task <FaPlus size={18}  /> </ button>
+    </div>
+  )
+}
+
+export default AddTask
---
+++ Challenge_frontend/toodolist/app/components/TodoList.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const TodoList = () => {
+ return <div className="overflow-x-auto">
+ <table className="table">
+   
+   <thead>
+     <tr>
+       <th></th>
+       <th>Name</th>
+       <th>Job</th>
+       <th>Favorite Color</th>
+     </tr>
+   </thead>
+   <tbody>
+  
+     <tr>
+       <th>1</th>
+       <td>Cy Ganderton</td>
+       <td>Quality Control Specialist</td>
+       <td>Blue</td>
+     </tr>
+    
+   </tbody>
+ </table>
+</div>
+}
+
+export default TodoList
